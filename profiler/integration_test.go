90bb5220b7695298fec7c8e22b50f432661a5896
Jianqiao Li <jianqiaoli@jianqiaoli.svl.corp.google.com>
2017-09-29 12:16:50 -0700

    test change
    Change-Id: I7ea7fb6b505edfe256d8f0dd38ed4557ce28d00a

--- profiler/integration_test.go
+++ profiler/integration_test.go
@@ -88,6 +88,7 @@ mkdir -p $GOCLOUD_HOME
 git clone https://code.googlesource.com/gocloud $GOCLOUD_HOME
 
 cd $GOCLOUD_HOME
+git checkout profiler-test
 git reset --hard {{.Commit}}
 go get -v ./...
 
@@ -97,7 +98,7 @@ go run profiler/busybench/busybench.go --service="{{.Service}}"
 
 const dockerfileFmt = `FROM golang
 RUN git clone https://code.googlesource.com/gocloud /go/src/cloud.google.com/go \
-    && cd /go/src/cloud.google.com/go && git reset --hard %s \
+    && cd /go/src/cloud.google.com/go && git checkout profiler-test && git reset --hard %s \
     && go get -v cloud.google.com/go/... && go install -v cloud.google.com/go/profiler/busybench
 CMD ["busybench", "--service", "%s"]
  `
